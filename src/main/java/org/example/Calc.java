cde1fdcddabdf737f7b4356c65a4f647fd4dd398
totoseel <ojinsup@naver.com>
2026-06-01 09:53:32 +0900

    Initial Commit

---
+++ src/main/java/org/example/Calc.java
@@ -0,0 +1,20 @@
+package org.example;
+
+
+public class Calc {
+    public int getSum(int a, int b){
+        return 0;
+    }
+
+    public int getGop(int a, int b){
+        return 0;
+    }
+
+    public int getZegop(int a, int b){
+        return 0;
+    }
+
+    public int getMinus(int a, int b){
+        return 0;
+    }
+}
