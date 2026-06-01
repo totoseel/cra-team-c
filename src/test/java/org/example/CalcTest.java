3e902544dfc2b87d02ef6b691ca508da131f0f62
chloelee7 <chloe.lee7@emcore.kr>
2026-06-01 10:05:23 +0900

    feature: add getGop

--- src/test/java/org/example/CalcTest.java
+++ src/test/java/org/example/CalcTest.java
@@ -1,11 +1,26 @@
 package org.example;
 
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.junit.jupiter.MockitoExtension;
+import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-@ExtendWith(MockitoExtension.class)
 class CalcTest {
+    private final Calc calc = new Calc();
 
+    @Test
+    void getGopReturnsProduct() {
+        assertEquals(12, calc.getGop(3, 4));
+    }
+
+    @Test
+    void getGopReturnsZeroWhenAnyOperandIsZero() {
+        assertEquals(0, calc.getGop(0, 5));
+        assertEquals(0, calc.getGop(5, 0));
+    }
+
+    @Test
+    void getGopReturnsNegativeWhenOnlyOneOperandIsNegative() {
+        assertEquals(-12, calc.getGop(-3, 4));
+        assertEquals(-12, calc.getGop(3, -4));
+    }
 }
