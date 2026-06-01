ee6b90dbdb7700c100229ff71de489273ad28c29
totoseel <ojinsup@naver.com>
2026-06-01 10:09:09 +0900

    Add getSum

--- src/test/java/org/example/CalcTest.java
+++ src/test/java/org/example/CalcTest.java
@@ -1,11 +1,20 @@
 package org.example;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.*;
 
 @ExtendWith(MockitoExtension.class)
 class CalcTest {
+    private Calc calc = new Calc();
 
+    @Test
+    void test_두_정수의_합() {
+        assertThat(calc.getSum(2, 3)).isEqualTo(5);
+        assertThat(calc.getSum(-1, 5)).isEqualTo(4);
+        assertThat(calc.getSum(0, 0)).isEqualTo(0);
+    }
 }
