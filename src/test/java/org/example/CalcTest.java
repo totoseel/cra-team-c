66ac64118b8ac4a349d791c561ab09b181d9abe1
totoseel <ojinsup@naver.com>
2026-06-01 10:11:54 +0900

    Add getSun Rollback

--- src/test/java/org/example/CalcTest.java
+++ src/test/java/org/example/CalcTest.java
@@ -9,12 +9,4 @@ import static org.assertj.core.api.Assertions.*;
 
 @ExtendWith(MockitoExtension.class)
 class CalcTest {
-    private Calc calc = new Calc();
-
-    @Test
-    void test_두_정수의_합() {
-        assertThat(calc.getSum(2, 3)).isEqualTo(5);
-        assertThat(calc.getSum(-1, 5)).isEqualTo(4);
-        assertThat(calc.getSum(0, 0)).isEqualTo(0);
-    }
 }
